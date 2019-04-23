846eb051084e7c1fdb441e70371ab97cf93ff62f
Manson36 <469211209@qq.com>
2019-04-23 19:47:07 +0800

    [Review]family1

---
+++ project02/main.go
@@ -0,0 +1,103 @@
+package utils
+
+import "fmt"
+
+type FamilyAccount struct {
+	key int
+	loop bool
+	balance float64
+	money float64
+	note string
+	details string
+	flag bool
+}
+
+func NewFamilyAccount () *FamilyAccount {
+
+	return &FamilyAccount{
+		key:0,
+		loop:true,
+		balance:10000.0,
+		money:0.0,
+		note:"",
+		flag:true,
+		details:"收支\t账户余额\t收支金额\t说  明",
+	}
+}
+
+func (this *FamilyAccount)showDetails() {
+
+	fmt.Println("------当前的收支记账明细------")
+	if this.flag {
+		fmt.Println("当前没有收支明细，来一笔吧")
+	}else {
+		fmt.Println(this.details)
+	}
+}
+
+func (this *FamilyAccount)inComes() {
+	fmt.Println("本次收入金额")
+	fmt.Scanln(&this.money)
+	this.balance += this.money
+	fmt.Println("本次收入说明：")
+	fmt.Scanln(&this.note)
+	this.details += fmt.Sprintf("\n收入\t%v\t%v\t%v",this.balance,this.money,this.note)
+	this.flag = true
+}
+
+func (this *FamilyAccount)pay() {
+	fmt.Println("本次支出金额")
+	fmt.Scanln(&this.money)
+	if this.money > this.balance{
+		fmt.Println("余额不足")
+		//break
+	}
+	this.balance -= this.money
+	fmt.Println("本次支出说明")
+	fmt.Scanln(&this.note)
+	this.details += fmt.Sprintf("\n支出\t%v\t%v\t%v",this.balance,this.money,this.note)
+	this.flag =true
+}
+
+func (this *FamilyAccount)exit() {
+	fmt.Println("你确定要退出吗？y/n")
+	choice := ""
+	for {
+		fmt.Scanln(&choice)
+		if choice == "y" || choice == "n"{
+			break
+		}
+		fmt.Println("你输入的有误")
+	}
+	if choice == "y"{
+		this.loop = false
+	}
+}
+
+func (this *FamilyAccount)MainMenu() {
+	for {
+		fmt.Println("-----------家庭收支记账软件-----------")
+		fmt.Println("            1 收支明细")
+		fmt.Println("            2 登记收入")
+		fmt.Println("            3 登记支出")
+		fmt.Println("            4 退出软件")
+		fmt.Print("请选择（1-4）：")
+		fmt.Scanln(&this.key)
+
+		switch this.key {
+		case 1:
+			this.showDetails()
+		case 2:
+			this.inComes()
+		case 3:
+			this.pay()
+		case 4:
+			this.exit()
+		default:
+			fmt.Println("请输入正确的选项")
+		}
+		if !this.loop {
+			break
+		}
+	}
+}
