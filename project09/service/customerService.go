216145463e4ab44b926d7bb45ff84b9f327ed663
Manson36 <469211209@qq.com>
2019-04-30 20:23:46 +0800

    [Review]Practice09

---
+++ project09/service/customerService.go
@@ -0,0 +1,10 @@
+package service
+
+import "github.com/review/project09/model"
+
+type CustomerService struct {
+	customers []model.Customer
+	customerNum int
+}
+
+
---
+++ project09/view/customerView.go
@@ -0,0 +1,54 @@
+package view
+
+import (
+	"fmt"
+)
+
+type customerView struct {
+	key string
+	loop bool
+}
+
+//显示主菜单
+func (this *customerView)mainMenu() {
+
+	for {
+		fmt.Println("--------客户信息管理软件---------")
+		fmt.Println("           1.添加客户")
+		fmt.Println("           2.修改客户")
+		fmt.Println("           3.删除客户")
+		fmt.Println("           4.客户列表")
+		fmt.Println("           5.退出")
+		fmt.Print("请选择（1-5）：")
+
+		fmt.Scanln(&this.key)
+		switch this.key {
+		case "1":
+			fmt.Println("添加客户")
+		case "2":
+			fmt.Println("修改客户")
+		case "3":
+			fmt.Println("删除客户")
+		case "4":
+			fmt.Println("客户列表")
+		case "5":
+			this.loop = false
+		default:
+			fmt.Println("您的输入有误，请重新输入")
+		}
+		if !this.loop {
+			break
+		}
+	}
+	fmt.Println("你退出了客户管理软件")
+}
+
+func main()  {
+
+	customerView := customerView{
+		key:"",
+		loop:true,
+	}
+
+	customerView.mainMenu()
+}
